ccaecd7f91fc115a4f792d4fa1b46d0b898a1769
matheuspb95 <matheuspb95@gmail.com>
2016-02-22 15:39:20 -0400

    first commit

---
+++ Combos/Assets/AnimatorStatesController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AnimatorStatesController : MonoBehaviour {
+	public List<string> Animations = new List<string>();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetAnimation(string animation){
+		for(int i = 0; i < Animations.Count; i++){
+			if(Animations[i] == animation){
+				Debug.Log(animation);
+				GetComponent<Animator>().SetBool(animation, true);
+			}else{
+				GetComponent<Animator>().SetBool(animation, false);
+			}
+		}
+	}
+}
---
+++ Combos/Assets/Platformer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platformer : MonoBehaviour {
+	public enum States {
+		Idle,
+		Walking,
+		Jumping
+	}
+
+	public States state;
+
+	public float JumpForce;
+	public float Velocity;
+
+	private Movement move;
+
+	// Use this for initialization
+	void Start () {
+		state = States.Idle;
+		move = GetComponent<Movement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Jump(){
+		state = States.Jumping;
+		move.Force(new Vector2(0,JumpForce * 100));
+	}
+
+	public void WalkRight(){
+		move.MoveForward(Velocity);
+		state = States.Walking;
+		GetComponent<SpriteRenderer>().flipX = false;
+	}
+
+	public void WalkLeft(){
+		move.MoveForward(-Velocity);
+		state = States.Walking;
+		GetComponent<SpriteRenderer>().flipX = true;
+	}
+}
